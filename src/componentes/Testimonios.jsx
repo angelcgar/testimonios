f9c55852913c905c4d70d34b138c2a67dccfa970
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2023-10-31 09:46:46 -0600

    añadir archivos desde github

---
+++ src/componentes/Testimonios.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
+import '../hojas-de-estilo/Testimonio.css'
+
+// eslint-disable-next-line no-unused-vars
+function Testimonio(props) {
+  return (
+    // comenario
+    <div className="contenedor-testimonio">
+      <img
+        className="imagen-testimonio"
+        src={require(`../imagenes/${props.imagen}.png`)}
+        alt={(`Foto de ${props.nombre}`)}
+      />
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">
+          <strong>{props.nombre}</strong> en {props.pais}
+        </p>
+        <p className="cargo-testimonio">
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className="texto-testimonio">"{props.testimonio}"</p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonio;
